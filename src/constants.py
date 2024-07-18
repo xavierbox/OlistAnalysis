2c4b9dc571729854093a9a8cafd40975c476d06c
xavierbox <42065172+xavierbox@users.noreply.github.com>
2024-07-18 19:32:53 +0000

    Load and understand the data. Part 1

---
+++ src/constants.py
@@ -0,0 +1,12 @@
+KNOWN_DATE_COLUMNS = [
+    "shipping_limit_date",
+    "review_creation_date",
+    "review_answer_timestamp",
+    "order_purchase_timestamp",
+    "order_approved_at",
+    "order_delivered_carrier_date",
+    "order_delivered_customer_date",
+    "order_estimated_delivery_date",
+]
+
+date_format = "%Y/%m/%d"
